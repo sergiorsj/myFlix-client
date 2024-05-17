5c534e9b6e0ff26afac817bac800284a2d63a733
sergiorsj11 <sergiorsj11@gmail.com>
2024-05-16 23:36:00 -0300

    folder components exerc 3.3
    main-view
    movie-card
    movie-view

---
+++ components/MainView/main-view.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export const MainView = () => {
+    const [movies, setMovies] = useState([
+      {
+        title: 'Shutter Island',
+        director: 'Martin Scorsese',
+        genre:'Suspense-Thriller'
+      },
+      {
+        title: 'The Fugitive',
+        director: 'Andrew Davis',
+        genre: 'Suspense-Thriller'
+      },
+      {
+        title: 'The Shack',
+        director: 'Stuart Hazeldine',
+        genre: 'Feel-Good'
+      },
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    );
+  }
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={() => {
+            setSelectedMovie(movie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
