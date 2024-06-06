8c37ab498461c6247db5e22570d7572397803689
sergiorsj11 <sergiorsj11@gmail.com>
2024-06-06 20:48:18 -0300

    exer 3,7
    working w/ John

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -3,8 +3,10 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { Container } from "react-bootstrap";
 import { SignUpView } from "../signup-view/signup-view";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 export const MainView = () => {
@@ -23,46 +25,98 @@ export const MainView = () => {
   } )
 
 
-  if (!user) {
-    return <Container><h1>Log in</h1>
-      <LoginView onLoggedIn={(user) => setUser(user)} />
-    <br/> Or <br/>
-    <h1>Sign Up</h1>
-    <SignUpView/>
-    </Container>;
-  }
+  // if (!user) {
+  //   return <Container><h1>Log in</h1>
+  //     <LoginView onLoggedIn={(user) => setUser(user)} />
+  //   <br/> Or <br/>
+  //   <h1>Sign Up</h1>
+  //   <SignUpView/>
+  //   </Container>;
+  // }
 
-  if (selectedMovie) {
-    return (
-      <div>
-      <button
-          onClick={() => {
-            setUser(null);
-          }}
-        >
-          Logout
-        </button>
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      /></div>
-    );
-  }
-  if (movies.length === 0) {
-    return <div>
-    <button
-        onClick={() => {
-          setUser(null);
-        }}
-      >
-        Logout
-      </button>
-      The list is empty!</div>;
-  }
+  // if (selectedMovie) {
+  //   return (
+  //     <div>
+  //     <button
+  //         onClick={() => {
+  //           setUser(null);
+  //         }}
+  //       >
+  //         Logout
+  //       </button>
+  //     <MovieView
+  //       movie={selectedMovie}
+  //       onBackClick={() => setSelectedMovie(null)}
+  //     /></div>
+  //   );
+  // }
+  // if (movies.length === 0) {
+  //   return <div>
+  //   <button
+  //       onClick={() => {
+  //         setUser(null);
+  //       }}
+  //     >
+  //       Logout
+  //     </button>
+  //     The list is empty!</div>;
+  // }
 
   return (
+    <BrowserRouter>
     <Row>
-      <button
+    <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <SignUpView />
+                  </Col>
+                )}
+              </>
+
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
+                  </Col>
+                )}
+              </>
+
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <>
+                    {movies.map((movie) => (
+                      <Col className="mb-4" key={movie._id} md={3}>
+                        <MovieCard movie={movie} />
+                      </Col>
+                    ))}
+                  </>
+                )}
+              </>
+            }
+          />
+      {/* <button
         onClick={() => {
           setUser(null);
         }}
@@ -77,7 +131,9 @@ export const MainView = () => {
             setSelectedMovie(movie);
           }}
         /></Col>
-      ))}
+      ))} */}
+      </Routes>
     </Row>
+    </BrowserRouter>
   );
 };
